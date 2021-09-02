bbbfe5e6c9e71d2ca0190077435758f5422b1e4d
Peter Gassner <peter@gassner.io>
2021-09-02 14:39:39 +0200

    fix: Don't run _onExternalLoginHook more than once

--- atmosphere-packages/webpack-dev-middleware/dev-server.js
+++ atmosphere-packages/webpack-dev-middleware/dev-server.js
@@ -301,6 +301,7 @@ if (Meteor.isServer && Meteor.isDevelopment) {
                     Accounts._options = Object.assign({}, accountsOptions)
                     Accounts._validateLoginHook.callbacks = Object.assign({}, _validateLoginHookCallbacks)
                     delete Accounts._onCreateUserHook
+                    delete Accounts._onExternalLoginHook
                 }
             } else {
                 isServerStateCached = true;
